a31bf6d551d87f3000f84cb1d014a0eb1de6ffe3
Ben R <benrotolo@gmail.com>
2017-05-01 20:49:23 -0400

    updates to models

--- termproject/models.py
+++ termproject/models.py
@@ -15,18 +15,18 @@ class Note(models.Model):
     velocity = models.DecimalField(max_digits=20, decimal_places=10, null=True, blank=True)
     channel = models.DecimalField(max_digits=20, decimal_places=10, null=True, blank=True)
     avg_rating = models.DecimalField(max_digits=20, decimal_places=10, null=True, blank=True)
-    song = models.CharField(max_length=200)
+    song = models.ForeignKey('termproject.Song', db_index=True, max_length=200, null=True, blank=True)
 
 
 class Rating(models.Model):
     id = models.AutoField(primary_key=True)
     note = models.ForeignKey('termproject.Note')
-    userID = models.ForeignKey('auth.User')
-    value = models.DecimalField(max_digits=20, decimal_places=10, null=True, blank=True)
+    user = models.ForeignKey('auth.User')
+    rating = models.DecimalField(max_digits=20, decimal_places=10, null=True, blank=True)
 
 
 class Song(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
     model = models.CharField(max_length=200)
-    date = models.DateTimeField(default=timezone.now)
+    creation_date = models.DateTimeField(auto_now_add=True)
